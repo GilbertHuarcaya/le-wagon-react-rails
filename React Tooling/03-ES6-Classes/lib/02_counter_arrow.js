0cccc8ab68f67033bba8578e3688a7ae75b544fa
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-02-14 14:20:47 -0500

    terminado

--- React Tooling/03-ES6-Classes/lib/02_counter_arrow.js
+++ React Tooling/03-ES6-Classes/lib/02_counter_arrow.js
@@ -1,2 +1,23 @@
+class Counter {
+  constructor(text) {
+    // TODO: build an internal Map of word => occurrences.
+    this.text = text;
+  }
 
+  occurrences(word) {
+    // TODO: return the number of occurrences
+    const splited = this.text.split(" ");
+    let sum = 0;
+    splited.map((e) => {
+      if (e.toLowerCase().match(`^${word.toLowerCase()}$`)) {
+        sum += 1;
+        return 'done';
+      }
+      sum += 0;
+      return 'done';
+    });
+    return sum;
+  }
+}
 
+module.exports = Counter;
